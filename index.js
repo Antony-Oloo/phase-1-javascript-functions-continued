641cf4af7c4e8b4bc893ee0ec5efece94b10b6e3
antony <antoloo254@gmail.com>
2024-09-25 12:18:00 +0100

    Saturday fun, Monday work, and wrap adjective functions implemented

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+
+// function saturdayFun(activity = 'roller-skate'){
+//     return('This Saturday, I want to ${activity}!');
+// }
+// function mondayWork(activity = 'go to the office'){
+//     return('This Monday, I will ${activity}');
+// }
+// function wrapAdjective(){
+//     function wrapAdjective(flair = '*') {
+//         return function(adjective = 'special') {
+//           return `You are ${flair}${adjective}${flair}!`;
+//         };
+//     }
+    
+// }
+// Function to define activities on Saturday
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function to define activities on Monday
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Function to wrap adjectives with visual flair
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+
+
